e173df5d9b521d0d579acc3a97de04828bc8b403
[DeveshPurohit] <purohitdev08@gmail.com>
2022-03-17 15:43:01 +0530

    about page setup

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import styles from '../styles/about.module.css'
 
 function about() {
   return (
-    <div>This is about page</div>
+    <div className={styles.container}>
+      <h1 className={styles.head}>About Hunting Coder</h1>  
+      <h2>Introduction</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nulla sint recusandae hic eos nam, ex debitis illo magnam rerum, nihil inventore placeat. Temporibus eligendi quidem fugit consectetur enim ipsa rem libero maiores earum doloremque molestiae eveniet quasi debitis esse alias cumque architecto, aperiam quis dolore. Porro deserunt velit natus iste doloribus quos veniam unde, deleniti in corporis. Doloribus voluptatum quod illo dolorem doloremque amet porro veniam quam incidunt tempore dicta cumque accusantium at, officia suscipit magnam? Dolor odit quidem architecto soluta voluptates impedit cumque necessitatibus ipsam! Repudiandae provident quae nemo ea nulla, earum reprehenderit dolorem natus nesciunt vitae totam.</p>
+      <h2>Services Offerd</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque perferendis fuga quam eligendi tenetur deserunt ipsum, doloremque impedit blanditiis deleniti illum quis saepe, repellat consequuntur eum dolorem cupiditate! Deserunt sequi error sit commodi in! Vitae consequatur ex omnis inventore quis ab cum officiis maiores pariatur molestiae autem voluptatum, repellendus sequi alias earum? Illum, quis. Optio laborum aperiam repellendus omnis aliquam nemo repudiandae atque laboriosam quia eos laudantium reiciendis veniam, ipsa aut ex debitis modi eveniet quaerat officia facere nisi assumenda!</p>
+      <p>We offer following services :-</p>
+      <ul>
+        <li>srevice 1</li>
+        <li>srevice 1</li>
+        <li>srevice 1</li>
+        <li>srevice 1</li>
+        <li>srevice 1</li>
+        <li>srevice 1</li>
+        <li>srevice 1</li>
+        <li>srevice 1</li>
+      </ul>
+      <h2>Contact Us</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime rerum tempore a totam modi voluptatum, quod facere voluptates similique iure placeat praesentium explicabo, impedit voluptas ab aperiam incidunt provident dignissimos ea obcaecati, sequi fugit odio eaque! Provident nemo iure hic magni neque! Voluptatibus a velit nemo aperiam, inventore nisi maxime accusantium earum amet, dolores perferendis harum nesciunt blanditiis porro! Neque blanditiis iusto earum labore veritatis rerum doloribus! Neque, excepturi perspiciatis.</p>
+    </div>
   )
 }
 
